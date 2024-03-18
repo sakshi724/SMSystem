ae9a2b981077a2194c923dacaa0b2666262cbc18
sakshi724 <110465265+sakshi724@users.noreply.github.com>
2024-03-18 14:38:21 +0530

    Update dbconnection.php

--- SMSystem/studentms/includes/dbconnection.php
+++ SMSystem/studentms/includes/dbconnection.php
@@ -3,7 +3,7 @@ define('DB_HOST','smydb.mysql.database.azure.com');
 define('DB_USER','sakshi');
 define('DB_PASS','smeshram@1');
 define('DB_NAME','studentmsdb');
-// Establish database connection.
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));

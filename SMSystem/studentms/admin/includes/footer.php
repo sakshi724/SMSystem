157ca888aff14442f2216fb422b4c67e9c10ee64
sakshi724 <110465265+sakshi724@users.noreply.github.com>
2024-03-18 14:39:43 +0530

    Update footer.php

--- SMSystem/studentms/admin/includes/footer.php
+++ SMSystem/studentms/admin/includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block" style="color: blue;">Student  Management System</span>
-              
+              <iframe  src="https://webchat.botframework.com/embed/StudentManagementSystem1-bot?s=q51yiwIpFqA.59QZyYoqrWoqH4Y_ZzIQdA583k6AwyOfhW7bgORfSEk" style="height: 502px; max-height: 502px;"></iframe></div>
             </div>
           </footer>

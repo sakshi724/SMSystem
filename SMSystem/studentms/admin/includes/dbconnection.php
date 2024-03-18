3f39952f24cbb449e4f37c5e4cd4b175522ec948
sakshi724 <110465265+sakshi724@users.noreply.github.com>
2024-03-18 14:36:01 +0530

    Update dbconnection.php

--- SMSystem/studentms/admin/includes/dbconnection.php
+++ SMSystem/studentms/admin/includes/dbconnection.php
@@ -1,7 +1,7 @@
 <?php 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','smydb.mysql.database.azure.com');
+define('DB_USER','sakshi');
+define('DB_PASS','smeshram@1');
 define('DB_NAME','studentmsdb');
 try
 {
